0394429f35d7ce477c9c2921fafd958ee0744945
rxsalad <richard.xue@salad.com>
2025-07-01 15:46:48 -0700

    update the recipe

--- gromacs-srcg/start.sh
+++ gromacs-srcg/start.sh
@@ -4,6 +4,5 @@
 echo -e "\nRunning Jupyter Lab on port $JUPYTERLAB_PORT ..."
 jupyter lab --no-browser --port=$JUPYTERLAB_PORT --ip=* --allow-root  --NotebookApp.token='' --NotebookApp.password='' &
 
-# Run the main script and sleep to keep the container alive
-echo -e "\nRunning main_salad.py ..."
-python main_salad.py
+# Keep the container alive
+sleep infinity
